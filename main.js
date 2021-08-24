220e5846f26ce492dc3d9126457cb54fbde64474
Scott Stossel <stoss96@gmail.com>
2021-08-23 22:08:46 -0400

    start screen and timer

--- main.js
+++ main.js
@@ -1,29 +1,65 @@
 //start screen
+const startGameBtn = document.getElementById("start");
+const startScreen = document.getElementById("game-intro");
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
 
-window.onload = () => {
-    document.getElementById('start').onclick = () => {
-        startGame();
-    };
 
-    function startGame() {
-        const img = new Image();
-        img.src = '/images/Desert/Desert1.png';
-        img.onload = function() {
-            ctx.drawImage(img,0,0,700,500);
-        }
-    }
+//remove background on click
+function removeBG() {
+  startScreen.parentNode.removeChild(startScreen);
 }
 
-const canvas = document.querySelector('#canvas');
-const ctx = canvas.getContext('2d');
+//start game after click
+// startGame(levelData[0])
+//5 second timer
 
-// const img = new Image();
-// img.src = '/images/Desert.png';
 
-img.addEventListener('load', function() {
-    ctx.drawImage(img,0,0,700,500);
-    startGame();
+function timer() {
+  let sec = 5;
+  let timer = setInterval(function() {
+    document.getElementById('timer-display').innerHTML = 'Timer: '+ sec;
+    sec--;
+    if (sec < 0) {
+      gameOver();
+    }
+  }, 1000);
+}
+
+function startGame(level) {
+
+  removeBG();
+  const img = new Image();
+  img.src = "/images/Desert/Desert1.png";
+  img.onload = function () {
+    ctx.drawImage(img, 0, 0, 700, 500);
+  };
+}
+
+//generate random letter for the level
+function randomLetter() {
+    // let letter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+    let letter = String.fromCharCode(Math.floor(Math.random() * (90 - 65) + 65));
+    return letter;
+}
+
+function gameOver() {
+  alert('you lose');
+}
+
+let lives = 3;
+
+
+document.addEventListener('keydown', (e) => {
+  currentLetter = randomLetter();
+  
+  document.getElementById('random-letter').innerHTML = 'Letters: ' + currentLetter;
+  // draw the letter
+  // set timer
+  timer();
+  if (e.key == currentLetter) {
+    alert('same key')
+  } else {
+    alert('wrong key')
+  }
 })
-
-const startGameBtn = document.querySelector('btn');
-
