de44179be7f517d7414acd5078adbd7d9f42e593
Scott Stossel <stoss96@gmail.com>
2021-08-25 10:13:04 -0400

    working timer and key checker

---
+++ game.js
@@ -0,0 +1,8 @@
+//variables
+
+const startGameBtn = document.getElementById('start');
+const startScreen = document.getElementById('game-intro');
+let lives = 3;
+const letterInput = document.getElementById('input');
+const letterDisplay = document.getElementById('display-letter');
+
--- main.js
+++ main.js
@@ -1,9 +1,25 @@
-//start screen
+//variables
 const startGameBtn = document.getElementById("start");
 const startScreen = document.getElementById("game-intro");
-const canvas = document.querySelector("#canvas");
-const ctx = canvas.getContext("2d");
+// const timerElement = document.getElementById('timer-display');
+// const letterInput = document.getElementById('input');
+const letterDisplay = document.getElementById('random-letter');
+let letter = '';
+let lives = 3;
 
+
+
+//generate random letter for the level
+function randomLetter() {
+  // let letter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+  return String.fromCharCode(Math.floor(Math.random() * (90 - 65) + 65));
+}
+
+//adding letter to DOM
+function addLetterToDOM() {
+  randomLetter = randomLetter();
+  letterDisplay.innerHTML = randomLetter;
+}
 
 //remove background on click
 function removeBG() {
@@ -11,55 +27,71 @@ function removeBG() {
 }
 
 //start game after click
-// startGame(levelData[0])
+
+
 //5 second timer
 
-
 function timer() {
+  // alert('timer start');
   let sec = 5;
-  let timer = setInterval(function() {
-    document.getElementById('timer-display').innerHTML = 'Timer: '+ sec;
+  let timer = setInterval(function () {
+    document.getElementById("timer-display").innerHTML = "Timer: " + sec;
     sec--;
-    if (sec < 0) {
-      gameOver();
+    if (sec === 0) {
+      lives -= 1;
+      document.getElementById('lives').innerHTML = "Lives: " + lives;
+      clearInterval(timer);
+      sec = 0;
+      document.getElementById('timer-display').innerHTML = "Timer: " + sec;
+      
     }
   }, 1000);
 }
-
+//start game button
 function startGame(level) {
-
   removeBG();
-  const img = new Image();
-  img.src = "/images/Desert/Desert1.png";
-  img.onload = function () {
-    ctx.drawImage(img, 0, 0, 700, 500);
-  };
+  return start = true;
+  // level();
 }
 
-//generate random letter for the level
-function randomLetter() {
-    // let letter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
-    let letter = String.fromCharCode(Math.floor(Math.random() * (90 - 65) + 65));
-    return letter;
-}
-
+//game over
 function gameOver() {
   alert('you lose');
 }
 
-let lives = 3;
+document.getElementById('lives').innerHTML = "Lives: " + lives;
 
-
-document.addEventListener('keydown', (e) => {
-  currentLetter = randomLetter();
-  
-  document.getElementById('random-letter').innerHTML = 'Letters: ' + currentLetter;
-  // draw the letter
-  // set timer
-  timer();
-  if (e.key == currentLetter) {
-    alert('same key')
-  } else {
-    alert('wrong key')
+document.body.onkeyup = function(e) {
+  if (e.code == 'Space' && start == true) {
+    level();
+    return;
   }
-})
+}
+
+function level() {
+  // alert('level start');
+  addLetterToDOM();
+  timer();
+
+  document.addEventListener('keydown', (e) => {
+    if (e.key == letterDisplay.innerHTML.toLowerCase()) {
+        alert('correct key');
+        clearTimeout(timer);
+      }
+  })
+  }
+  
+
+  // document.addEventListener('input', (e) => {
+  //   //start timer
+  //   alert('started');
+  //   const text = e.target.value;
+  //   if (e.key === currentLetter) {
+  //     winLevel();
+  //     return;
+  //   } else {
+  //     loseLevel();
+  //     return;
+  //   }
+  // })
+
